9b03e456ae82ff4e8ca6b7dfadbf345e94c8eaa8
Subhasmita <nsubhasmit39@gmail.com>
2022-02-04 15:45:59 +0530

    loop

---
+++ loop/Text5time/Text5time.java
@@ -0,0 +1,12 @@
+package loop.Text5time;
+
+public class Text5time {
+
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=1;i<=n;++i) {
+			System.out.println("java is easy to learn");
+		}
+		}
+	}
+		
