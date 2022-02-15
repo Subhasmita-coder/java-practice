af80e399a2e71a14883294625e0e2a167e641bb0
Subhasmita <nsubhasmit39@gmail.com>
2022-02-15 09:49:31 +0530

    length array

---
+++ Array/Arraylength.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class Arraylength {
+
+	public static void main(String[] args) {
+		int arr[]= {10,20,30,40,50};
+		System.out.print("The lengthof the given array =");
+		System.out.print(arr.length);
+	}
+
+}
