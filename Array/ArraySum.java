aced52e4ffbaa5103419123ea8de030192712246
Subhasmita <nsubhasmit39@gmail.com>
2022-02-17 09:58:20 +0530

    ArraySum

---
+++ Array/ArraySum.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class ArraySum {
+
+	public static void main(String[] args) {
+		int array[]= {10,20,30,40,50};
+		int sum=0;
+		for (int i=0;i<array.length;i++) {
+			sum+=array[i];
+		}
+		System.out.println("sum of array elements="+sum);
+
+	}
+
+}
